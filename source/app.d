9057ebc3b153d3faa9e8742139c69f8b61476ca8
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-01 21:06:02 +0200

    EskomCalendar
    - Documented both constructors
    - Documented `ErrType` and its enum members
    - Any curl request goes through `doGet(string)` which wraps the `CurlException` in an `EskomCalendarException`
    - Typo fixes
    - Added unit test to dynamically test multiple areas and their respective schedules

--- source/app.d
+++ source/app.d
@@ -3,11 +3,26 @@ import std.json;
 import std.net.curl : get, CurlException;
 import std.conv : to;
 
-
+/** 
+ * Kind-of error that occurred
+ */
 public enum ErrType
 {
+    /** 
+     * If there are no areas available
+     */
     NO_AREAS_AVAILABLE,
-    NO_SCHEDULES_AVAILABLE
+
+    /** 
+     * If there were no schedules available
+     for the given area
+     */
+    NO_SCHEDULES_AVAILABLE,
+
+    /** 
+     * On error contacting the calendar server
+     */
+    NETWORK_ERROR
 }
 
 public final class EskomCalendarException : Exception
@@ -80,25 +95,65 @@ public class EskomCalendar
 {
     private string calendarServer;
 
+    /** 
+     * Constructs a new `EskomCalendar` using the provided
+     * custom server
+     *
+     * Params:
+     *   calendarServer = URL of the server to use
+     */
     this(string calendarServer)
     {
         import std.string : stripRight;
         this.calendarServer = stripRight(calendarServer, "/");
     }
 
+    /** 
+     * Constructs a new `EskomCalendar` using the
+     * default reference server
+     */
     this()
     {
         this("https://eskom-calendar-api.shuttleapp.rs/v0.0.1/");
     }
 
+    private final string doGet(string url)
+    {
+        try
+        {
+            return cast(string)get(url);
+        }
+        catch(CurlException e)
+        {
+            throw new EskomCalendarException(ErrType.NETWORK_ERROR, "Could not connect to server '"~calendarServer~"'");
+        }
+    }
+
+    /** 
+     * Get schedules from a given area
+     *
+     * Params:
+     *   area = the area to check for schedules
+     *   startTime = the lower bound to filter by (none by default)
+     *   finishTime = the upper bound to filter by (none by default)
+     * Returns: an array of `Schedule`(s)
+     */
     public Schedule[] getSchedules(string area, SysTime startTime = SysTime.min(), SysTime finishTime = SysTime.max())
     {
-        Schedule[] schecules;
+        Schedule[] schedules;
+
+        scope(exit)
+        {
+            version(unittest)
+            {
+                writeln("Exiting with '"~to!(string)(schedules.length)~" schedules for area '"~area~"' between '"~startTime.toSimpleString()~"' and '"~finishTime.toSimpleString()~"'");
+            }
+        }
 
         /** 
          * Fetch the schedules and parse
          */
-        string data = cast(string)get(calendarServer~"/outages/"~area);
+        string data = doGet(calendarServer~"/outages/"~area);
         JSONValue[] schedulesJSON = parseJSON(data).array();
         foreach(JSONValue schedule; schedulesJSON)
         {
@@ -106,7 +161,7 @@ public class EskomCalendar
 
             if(curSchedule.getStart() >= startTime && curSchedule.getFinish() <= finishTime)
             {
-                schecules ~= curSchedule;
+                schedules ~= curSchedule;
             }
         }
         
@@ -114,54 +169,47 @@ public class EskomCalendar
         
 
 
-        if(schecules.length == 0)
+        if(schedules.length == 0)
         {
             throw new EskomCalendarException(ErrType.NO_SCHEDULES_AVAILABLE, "No schedules for area '"~area~"'");
         }
 
-        return schecules;
+        return schedules;
     }
 
-    public Schedule[] getTodaySchdules(string area)
+    public Schedule[] getTodaySchedules(string area)
     {
         import std.datetime.systime :  Clock;
         import std.datetime.date : Date, DateTime;
         import std.datetime.systime : SysTime;
+        import core.thread : dur;
         
 
-        // Get just the date of today
+        // Get just the date of today (no time)
         Date todayDate = cast(Date)Clock.currTime();
 
+        // Get the start and end date+times but with time zeroed out
         SysTime startTime = cast(SysTime)todayDate;
         SysTime endTime = cast(SysTime)todayDate;
+
+        // Make end date+time 24 hours later
+        endTime += dur!("hours")(24);
 
         version(unittest)
         {
             writeln("startTime: ", startTime);
-        }
-        
-
-        import core.thread : dur;
-        endTime += dur!("hours")(24);
-
-
-        version(unittest)
-        {
             writeln("endTime: ", endTime);
         }
         
-
-        
-
         return getSchedules(area, startTime, endTime);
     }
 
-    public Schedule[] getSchdulesFrom(string area, SysTime startTime)
+    public Schedule[] getSchedulesFrom(string area, SysTime startTime)
     {
         return getSchedules(area, startTime);
     }
 
-    public Schedule[] getSchdulesUntil(string area, SysTime finishTime)
+    public Schedule[] getSchedulesUntil(string area, SysTime finishTime)
     {
         return getSchedules(area, SysTime.min(), finishTime);
     }
@@ -177,7 +225,7 @@ public class EskomCalendar
         import std.uri : encode;
         regex = encode(regex);
 
-        string data = cast(string)get(calendarServer~"/list_areas/"~regex);
+        string data = doGet(calendarServer~"/list_areas/"~regex);
         JSONValue[] areas = parseJSON(data).array();
 
         string[] areasStr;
@@ -201,7 +249,7 @@ unittest
 
     try
     {
-        Schedule[] schedules = calendar.getTodaySchdules("western-cape-worscester");
+        Schedule[] schedules = calendar.getTodaySchedules("western-cape-worscester");
         foreach(Schedule schedule; schedules)
         {
             writeln("Today: "~schedule.toString());
@@ -209,11 +257,27 @@ unittest
     }
     catch(EskomCalendarException e)
     {
+        writeln("Crashed with '"~e.toString()~"'");
         assert(false);
     }
-    catch(CurlException e)
+}
+
+unittest
+{
+    EskomCalendar calendar = new EskomCalendar();
+
+    /** 
+     * Get all areas available
+     * and take a subset of them
+     */
+    string[] areas = calendar.getAreas()[0..10];
+
+    /**
+     * Get the schedules per-each of them
+     */
+    foreach(string area; areas)
     {
-        writeln(e);
+        Schedule[] schedules = calendar.getSchedules(area);
     }
 }
 
@@ -233,11 +297,8 @@ unittest
     }
     catch(EskomCalendarException e)
     {
+        writeln("Crashed with '"~e.toString()~"'");
         assert(false);
-    }
-    catch(CurlException e)
-    {
-        writeln(e);
     }
 }
 
@@ -253,6 +314,7 @@ unittest
     }
     catch(EskomCalendarException e)
     {
+        writeln("Crashed with '"~e.toString()~"'");
         assert(false);
     }
 }
