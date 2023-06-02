123598594e71e8eac133153fbb3bf872a693be22
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-02 08:50:48 +0200

    EskomCalendarException
    - Documented `EskomCalendarException` and its constructors, fields and methods

--- source/eskomcalendar/exceptions.d
+++ source/eskomcalendar/exceptions.d
@@ -29,16 +29,35 @@ public enum ErrType
     NETWORK_ERROR
 }
 
+/** 
+ * Represents an error that occurs using the `EskomCalendar` API
+ */
 public final class EskomCalendarException : Exception
 {
+    /** 
+     * Kind-of error that occurred
+     */
     private ErrType errType;
 
+    /** 
+     * Constructs a new `EskomCalendarException` of the given
+     * kind-of error that occurred with an optional message
+     * which is emoty by default
+     *
+     * Params:
+     *   errType = the kind-of error
+     */
     this(ErrType errType, string msg = "")
     {
         this.errType = errType;
         super(to!(string)(errType)~(msg.length ? (": "~msg) : ""));
     }
 
+    /** 
+     * Gets the kind-of error that occurred
+     *
+     * Returns: the error as an `ErrType`
+     */
     public ErrType getError()
     {
         return errType;
