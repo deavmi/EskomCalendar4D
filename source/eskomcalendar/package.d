069c1872c4251b046b27c5130f26426d7115ac43
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-02 11:30:39 +0200

    Package
    - Documented all public imports

--- source/eskomcalendar/package.d
+++ source/eskomcalendar/package.d
@@ -1,5 +1,16 @@
 module eskomcalendar;
 
+/**
+ * The client API
+ */
 public import eskomcalendar.calendar;
+
+/**
+ * Scheduling-related definitions
+ */
 public import eskomcalendar.schedule;
+
+/**
+ * Error handling definitions
+ */
 public import eskomcalendar.exceptions;
